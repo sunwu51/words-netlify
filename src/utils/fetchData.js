d7d56e66125219944a7413504fc1c4e917c32fa4
Frank <sunwu51@126.com>
2022-12-15 19:06:13 +0800

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const secret = import.meta.env.VITE_SECRET || "demo";
 const herokuUrl = import.meta.env.VITE_API_URL || "";
 
-console.log(import.meta.env)
-
 /**
  * 获取单词本所有的单词，格式如下
  * @returns demo  [
